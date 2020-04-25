3a792caffeeb0ad3207dac45c949e81aada87c76
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-04-25 14:36:25 -0300

    Unit test for 2kN

--- tests/test_factorial2k.py
+++ tests/test_factorial2k.py
@@ -1,16 +1,26 @@
 from pytest import approx
 from src.factorial2k import effects_table_method, variation_allocation
 
-FACTORS = 2
-RESULTS = [[15], [45], [25], [75]]
+# 2K^2 FACTORIAL DESIGN TESTS #
+FACTORS_2K2 = 2
+RESULTS_2K2 = [[15], [45], [25], [75]]
+EFFECTS_2K2 = effects_table_method(FACTORS_2K2, RESULTS_2K2)
+ALLOCATION_2K2 = variation_allocation(EFFECTS_2K2.copy())
 
+def test_effects2k2():
+    assert EFFECTS_2K2 == [40.0, 20.0, 10.0, 5.0]
 
-def test_effects():
-    effects = effects_table_method(FACTORS, RESULTS)
-    assert effects == [40.0, 20.0, 10.0, 5.0]
+def test_variation2k2():
+    assert ALLOCATION_2K2 == approx([76, 19, 5], rel=1e-1, abs=1)
 
+# 2K^n FACTORIAL DESIGN TESTS #
+FACTORS_2KN = 3
+RESULTS_2KN = [[14], [22], [10], [34], [46], [58], [50], [86]]
+EFFECTS_2KN = effects_table_method(FACTORS_2KN, RESULTS_2KN)
+ALLOCATION_2KN = variation_allocation(EFFECTS_2KN.copy())
 
-def test_variation():
-    effects = effects_table_method(FACTORS, RESULTS)
-    allocation = variation_allocation(effects)
-    assert allocation == approx([76, 19, 5], rel=1e-1, abs=1e-1)
+def test_effects2kN():
+    assert EFFECTS_2KN == [40.0, 10.0, 5.0, 20.0, 5.0, 2.0, 3.0, 1.0]
+
+def test_variation2kN():
+    assert ALLOCATION_2KN == approx([18, 4, 71, 4, 1, 2, 0], rel=1e-1, abs=1)
