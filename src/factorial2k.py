f8ac0659d07315b0a44610294c4f7db66a0823bd
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-04-25 12:14:00 -0300

    Add variation allocation algorithm

--- src/factorial2k.py
+++ src/factorial2k.py
@@ -1,12 +1,27 @@
 import pyDOE2 as pd
 import numpy as np
 
-def effects_table_method(FACTORS, RESULTS):
-    image_column = np.ones((2 ** FACTORS, 1))
-    factorial_columns = pd.fracfact('a b ab')
-    results_column = np.array(RESULTS)
-    
+
+def effects_table_method(factors, results):
+    image_column = np.ones((2 ** factors, 1))
+    factorial_columns = pd.fracfact("a b ab")
+    results_column = np.array(results)
+
     sign_table = np.hstack((image_column, factorial_columns))
 
-    total = [float(np.dot(sign_table[:, i], results_column)/4) for i in range(2 ** FACTORS)]
+    total = [
+        float(np.dot(sign_table[:, i], results_column) / 4) for i in range(2 ** factors)
+    ]
     return total
+
+
+def variation_allocation(effects):
+    # The first elements represents q0, and is not important here
+    effects.pop(0)
+
+    allocation = [4 * effect * effect for effect in effects]
+    total_variation = sum(allocation)
+
+    percentages = [(100 * a) / total_variation for a in allocation]
+
+    return percentages
--- tests/test_factorial2k.py
+++ tests/test_factorial2k.py
@@ -1,8 +1,16 @@
-from src.factorial2k import effects_table_method
+from pytest import approx
+from src.factorial2k import effects_table_method, variation_allocation
+
+FACTORS = 2
+RESULTS = [[15], [45], [25], [75]]
+
 
 def test_effects():
-    FACTORS = 2
-    RESULTS = [[15], [45], [25], [75]]
-    assert effects_table_method(FACTORS, RESULTS) == [40.0, 20.0, 10.0, 5.0]
+    effects = effects_table_method(FACTORS, RESULTS)
+    assert effects == [40.0, 20.0, 10.0, 5.0]
 
 
+def test_variation():
+    effects = effects_table_method(FACTORS, RESULTS)
+    allocation = variation_allocation(effects)
+    assert allocation == approx([76, 19, 5], rel=1e-1, abs=1e-1)
